3b3ad97195c7f676d6dd181c81f4d0e9b37786a8
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-07-10 22:12:10 +0900

    20.7.4

--- src/main/java/com/hdmr1234/jlox/Lox.java
+++ src/main/java/com/hdmr1234/jlox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,7 @@ public class Lox {
 
         if (hadError)
             System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -56,11 +59,18 @@ public class Lox {
 
         if (hadError) return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
+
+        // System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
@@ -76,4 +86,5 @@ public class Lox {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
     }
+
 }
