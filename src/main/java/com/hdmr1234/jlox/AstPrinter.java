4f0d3ac396a6a7f36b38f700051fff640da9038b
maro525 <hdmrf.fjnm@gmail.com>
2020-08-08 22:08:59 +0900

    chapter 9

--- src/main/java/com/hdmr1234/jlox/AstPrinter.java
+++ src/main/java/com/hdmr1234/jlox/AstPrinter.java
@@ -1,61 +1,46 @@
 package com.hdmr1234.jlox;
 
- class AstPrinter implements Expr.Visitor<String> {
-     String print(Expr expr) {
-         return expr.accept(this);
-     }
+ // class AstPrinter implements Expr.Visitor<String> {
+ //     String print(Expr expr) {
+ //         return expr.accept(this);
+ //     }
 
-     @Override
-     public String visitAssignExpr(Expr.Assign expr) {
-         return parenthesize("=", expr.value);
-     }
+ //    @Override
+ //    public String visitVariableExpr(Expr.Variable expr) {
+ //        return expr.name.lexeme;
+ //     }
 
-     @Override
-     public String visitBinaryExpr(Expr.Binary expr) {
-         return parenthesize(expr.operator.lexeme, expr.left, expr.right);
-     }
+ //     @Override
+ //     public String visitLiteralExpr(Expr.Literal expr) {
+ //         if (expr.value == null)
+ //             return "nil";
+ //         return expr.value.toString();
+ //     }
 
-    @Override
-    public String visitVariableExpr(Expr.Variable expr) {
-        return expr.name.lexeme;
-     }
+ //     @Override
+ //     public String visitUnaryExpr(Expr.Unary expr) {
+ //         return parenthesize(expr.operator.lexeme, expr.right);
+ //     }
 
-     @Override
-     public String visitGroupingExpr(Expr.Grouping expr) {
-         return parenthesize("group", expr.expression);
-     }
+ //     private String parenthesize(String name, Expr... exprs) {
+ //         StringBuilder builder = new StringBuilder();
 
-     @Override
-     public String visitLiteralExpr(Expr.Literal expr) {
-         if (expr.value == null)
-             return "nil";
-         return expr.value.toString();
-     }
+ //         builder.append("(").append(name);
+ //         for (Expr expr : exprs) {
+ //             builder.append(" ");
+ //             builder.append(expr.accept(this));
+ //         }
+ //         builder.append(")");
 
-     @Override
-     public String visitUnaryExpr(Expr.Unary expr) {
-         return parenthesize(expr.operator.lexeme, expr.right);
-     }
+ //         return builder.toString();
+ //     }
 
-     private String parenthesize(String name, Expr... exprs) {
-         StringBuilder builder = new StringBuilder();
+ //     public static void main(String[] args) {
+ //         Expr expression = new Expr.Binary(
+ //                 new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)),
+ //                 new Token(TokenType.STAR, "*", null, 1),
+ //                 new Expr.Grouping(new Expr.Literal(45.67)));
 
-         builder.append("(").append(name);
-         for (Expr expr : exprs) {
-             builder.append(" ");
-             builder.append(expr.accept(this));
-         }
-         builder.append(")");
-
-         return builder.toString();
-     }
-
-     public static void main(String[] args) {
-         Expr expression = new Expr.Binary(
-                 new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(123)),
-                 new Token(TokenType.STAR, "*", null, 1),
-                 new Expr.Grouping(new Expr.Literal(45.67)));
-
-         System.out.println(new AstPrinter().print(expression));
-     }
-}
+ //         System.out.println(new AstPrinter().print(expression));
+ //     }
+// }
--- src/main/java/com/hdmr1234/jlox/Interpreter.java
+++ src/main/java/com/hdmr1234/jlox/Interpreter.java
@@ -30,6 +30,19 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
 
     @Override
+    public Object visitLogicalExpr(Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if (isTruthy(left)) return left;
+        } else {
+            if (!isTruthy(left)) return left;
+        }
+
+        return evaluate(expr.right);
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);
     }
@@ -62,7 +75,17 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
-    //     evaluate(stmt.expression);
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitIfStmt(Stmt.If stmt) {
+        if (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.thenBranch);
+        } else if (stmt.elseBranch != null) {
+            execute(stmt.elseBranch);
+        }
         return null;
     }
 
@@ -80,6 +103,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
             value = evaluate(stmt.initializer);
         }
         environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Void visitWhileStmt(Stmt.While stmt) {
+        while (isTruthy(evaluate(stmt.condition))) {
+            execute(stmt.body);
+        }
         return null;
     }
 
